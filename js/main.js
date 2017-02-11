6446f0d1182423c029dc74cb1eac44344575fb0d
laura <laura@lauramackenzie.net>
2017-02-10 20:57:10 -0500

    updated main.css index.html main.js

--- js/main.js
+++ js/main.js
@@ -6,44 +6,46 @@
 //
 // Allow users to click the images to see a larger version with more information.
 $(document).on('ready', function(){
-    // Place your code here, inside the document ready handler.
 
-    // Create a function called `searchImages()`. This function will handle the
-    // process of taking a user's search terms and sending them to Flickr for a
-    // response.
+  function searchImages(tags) {
+  //calling flicker API
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: tags,
+    tagmode: "any",
+    format: "json"
+  })
+  //getting images based on search criteria and dispaying in grid with extra information.
+    .done(function( data ) {
+      $('#images').empty();
+      $('h1.search-title').html(tags + " images....");
+      $.each( data.items, function( i, item ) {
+        var newItem = $('<div class="col-sm-6 col-md-4">');
+        var thumbnail = $('<div class="thumbnail">').appendTo(newItem);
+        var link = $('<a>').attr("href", item.link).appendTo(thumbnail);
+        $( "<img>" ).attr( "src", item.media.m ).appendTo(link);
+        $('<div class="author">').html(item.author).appendTo(thumbnail);
+        $('<div class="caption">').html(item.title).appendTo(thumbnail);
+        $('<div class="label">').html(item.label).appendTo(thumbnail);
+        $('<div class="date">').html(item.date_taken).appendTo(thumbnail);
 
-    // Inside the `searchImages()` function, the following things should happen:
+        
+        newItem.appendTo( "#images" );   
 
-        // 1.   Accept a string value called `tags` as an argument. Example:
-        //      `var searchPhotos = function(tags){`
-        //
-        // 2.   Define the location of the Flickr API like this:
-        //      `var flickrAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";`
-        //
-        // 3.   Construct a `$.getJSON()` call where you send a request object
-        //      including the tags the user submitted, and a `done()` handler
-        //      that displays and refreshes the content appropriately.
-        //
-        // 4.   Update the display to add the images to the list with the id
-        //      `#images`.
-
-    // Attach an event to the search button (`button.search`) to execute the
-    // search when clicked.
-
-        // When the Search button is clicked, the following should happen:
-        //
-        // 1.   Prevent the default event execution so the browser doesn't
-        //      Example: `event.preventDefault();`
-        //
-        // 2.   Get the value of the 'input[name="searchText"]' and use that
-        //      as the `tags` value you send to `searchImages()`.
-        //
-        // 3.   Execute the `searchImages()` function to fetch images for the
-        //      user.
-
-    // STRETCH GOAL: Add a "more info" popup using the technique shown on the
-    // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
-
-
+        //limiting number of photos
+        if ( i === 30 ) {
+          return false;
+        }
+      });
+    });
+  }
+  
+  
+  //button event handler reads user input and calls searchImage function
+  $('button.search').on('click', function(event) {
+    event.preventDefault();
+    var searchInfo = $(".navbar-form").find('input[name="searchText"]')[0];
+    searchImages(searchInfo.value);
+   });               
+                
 });
